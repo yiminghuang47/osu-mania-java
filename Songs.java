438343986f617fa9a106f061e3d43c032cad278b
yiminghuang47 <yiminghuang47@gmail.com>
2024-06-12 15:37:35 -0400

    changed song titles

--- Songs.java
+++ Songs.java
@@ -1,5 +1,5 @@
 public class Songs {
-    static SongInfo song1 = new SongInfo(1, "Freedom Dive",216, 1400,914,"C:\\Users\\yimin\\OneDrive\\Documents\\Coding Projects\\OSUJava\\songs\\Freedom Dive.wav");
-    static SongInfo song2 = new SongInfo(2, "ANiMA", 262, 660,337,"C:\\Users\\yimin\\OneDrive\\Documents\\Coding Projects\\OSUJava\\songs\\anima.wav");
+    static SongInfo song1 = new SongInfo(1, "xi - Freedom Dive",216, 1400,925,"C:\\Users\\yimin\\OneDrive\\Documents\\Coding Projects\\OSUJava\\songs\\Freedom Dive.wav");
+    static SongInfo song2 = new SongInfo(2, "xi - ANiMA", 262, 660,337,"C:\\Users\\yimin\\OneDrive\\Documents\\Coding Projects\\OSUJava\\songs\\anima.wav");
     
 }
