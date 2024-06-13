9ebeeaff3506180eb3591ea5e169676a3a785c1c
yiminghuang47 <yiminghuang47@gmail.com>
2024-06-13 13:45:52 -0400

    updated README.md

--- Songs.java
+++ Songs.java
@@ -2,5 +2,5 @@ public class Songs {
     static SongInfo song1 = new SongInfo(1, "xi - Freedom Dive",216, 1400,925,"songs\\Freedom Dive.wav","images\\Freedom Dive.png");
     static SongInfo song2 = new SongInfo(2, "xi - ANiMA", 262, 660,337,"songs\\anima.wav","images/anima.jpg");
     static SongInfo song3 = new SongInfo(3, "DJ Okawari - Flower Dance", 241, 560,789,"songs\\Flower Dance Trimmed.wav","images/Flower Dance.jpg");
-    static SongInfo song4 = new SongInfo(4,"Laur - Vindication", 276,100,480,"songs\\Vindication.wav","images/Vindication.png");
+    static SongInfo song4 = new SongInfo(4,"Laur - Vindication", 276,100,430,"songs\\Vindication.wav","images/Vindication.png");
 }
