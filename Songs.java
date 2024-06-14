516f2275509d305a003137eb609d8912049157ba
yiminghuang47 <yiminghuang47@gmail.com>
2024-06-13 23:00:58 -0400

    change file path format

--- Songs.java
+++ Songs.java
@@ -1,6 +1,6 @@
 public class Songs {
-    static SongInfo song1 = new SongInfo(1, "xi - Freedom Dive",216, 1400,946,"songs\\Freedom Dive.wav","images\\Freedom Dive.png");
-    static SongInfo song2 = new SongInfo(2, "xi - ANiMA", 262, 660,337,"songs\\anima.wav","images/anima.jpg");
-    static SongInfo song3 = new SongInfo(3, "DJ Okawari - Flower Dance", 241, 560,780,"songs\\Flower Dance Trimmed.wav","images/Flower Dance.jpg");
-    static SongInfo song4 = new SongInfo(4,"Laur - Vindication", 276,100,418,"songs\\Vindication.wav","images/Vindication.png");
+    static SongInfo song1 = new SongInfo(1, "xi - Freedom Dive",216, 1400,946,"songs/Freedom Dive.wav","images/Freedom Dive.png");
+    static SongInfo song2 = new SongInfo(2, "xi - ANiMA", 262, 660,337,"songs/anima.wav","images/anima.jpg");
+    static SongInfo song3 = new SongInfo(3, "DJ Okawari - Flower Dance", 241, 560,780,"songs/Flower Dance Trimmed.wav","images/Flower Dance.jpg");
+    static SongInfo song4 = new SongInfo(4,"Laur - Vindication", 276,100,418,"songs/Vindication.wav","images/Vindication.png");
 }
